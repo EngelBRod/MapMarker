b4eb8598f2ebd944c0cf68f642ea2700b810db11
EngelBRod <14808288+EngelBRod@users.noreply.github.com>
2023-11-08 12:06:13 -0500

    adding firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React, { useRef, useState } from "react";
-import Map from "./components/Map";
+import { addDoc, collection, doc, updateDoc  } from "@firebase/firestore";
 import LinkedList from "./utilities/LinkedList";
+import firebase from "./utilities/Firebase";
+import Map from "./components/Map";
 
 function App() {
   const [markers, setMarkers] = useState([]);
   const linkedList = useRef(null);
+  const ref= collection(firebase,"Quests");
+  const currentCollection = useRef('5psoSkPhMS50TZgBU4lQ');
+ 
 
 
   const formatData = (quest)=>{
@@ -19,11 +24,11 @@ function App() {
     linkedList.current = temp;
 
     temp =temp.head
-    let formatter = `{"Quest${temp.value.Quest}":{"latitude": ${temp.value.latitude},"longitude":${temp.value.longitude}},"next":null}`
+    let formatter = `{"Quest${temp.value.Quest}":{"latitude": ${temp.value.latitude},"longitude":${temp.value.longitude},"next":null}}`
     temp = temp.next; 
     // eslint-disable-next-line no-plusplus
     for(let i=1; i < linkedList.current.size ; i++){
-      const tempValue = `"Quest${temp.value.Quest}":{"latitude":${temp.value.latitude},"longitude":${temp.value.longitude}},"next":null`;
+      const tempValue = `"Quest${temp.value.Quest}":{"latitude":${temp.value.latitude},"longitude":${temp.value.longitude},"next":null}`;
       formatter = formatter.replace('null', `{${tempValue}}`);
       
       temp = temp.next; 
@@ -32,6 +37,30 @@ function App() {
     console.log(result);
     return result;
   }
+
+  const sendData = (data)=>{
+    
+    if(markers.length===0){
+      try {
+      addDoc(ref,data)
+      .then((res)=>{
+        currentCollection.current =res.id;
+      })
+      }catch(e){
+        console.log(e);
+      }
+    }else{
+      const refData = doc(firebase,"Quests",currentCollection.current);
+      try {
+        updateDoc(refData,data);
+      }catch(e){
+        console.log(e);
+      }
+    }
+
+ 
+  }
+
   const onMapClick = (e) => {
     setMarkers((current) => [
       ...current,
@@ -46,10 +75,13 @@ function App() {
       latitude:e.latLng.lat(),
       longitude:e.latLng.lng(),
     }
-    test.current = formatData(quest);
-
+  
+   const data = formatData(quest);
+   sendData(data);
 
   };
+
+
 
 
   return(
